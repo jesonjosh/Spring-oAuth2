a75c67f2e425700fd0721c8e10c759175b024f06
jesonjosh <jesonpj85@gmail.com>
2016-05-21 11:37:47 +0400

    Add files via upload

---
+++ src/main/java/com/seva/models/ShopTableDTO.java
@@ -0,0 +1,35 @@
+package com.seva.models;
+
+public class ShopTableDTO {
+
+	private String id;
+	private String name;
+	private String description;
+	private String capacity;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(String capacity) {
+		this.capacity = capacity;
+	}
+	
+	
+}
